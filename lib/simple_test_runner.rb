143e9f28900bae8222aaa3d0c67be05ab417a2b5
Jeff Roush <jeff@jeffroush.com>
2011-03-15 15:54:49 -0700

    require. Not require_relative. We add the lib directory to the load path in bin/simple_test_runner.

--- lib/simple_test_runner.rb
+++ lib/simple_test_runner.rb
@@ -1,10 +1,10 @@
 #!/home/jeff/.rvm/rubies/ruby-1.9.2-p0/bin/ruby
 require "rubygems"
 require "bundler/setup"
-require_relative 'runner_opts'
 require 'rb-inotify'
-
 require 'io/wait'   # for io.ready?
+
+require 'runner_opts'
 
 # Bundler.require        # See http://technotales.wordpress.com/2010/08/22/bundler-without-rails/
 
@@ -13,23 +13,15 @@ require 'io/wait'   # for io.ready?
 
 module SimpleTestRunner
 
+  # Top-level class
   class TestRunner
 
+    # Initialized gets passed ARGv
     def initialize args = []
       @args = args
     end
 
-    def make_config_file
-      if File.exists? @options.config_file_name
-        puts "Are you sure? (y for yes)"
-        a = gets.strip.downcase
-        return unless a == "y"
-      end
-      File.open(@options.config_file_name, 'w+') do |file| 
-        file.puts @options.to_yaml
-      end
-    end
-
+    # This is the function that runs everything.
     def run
       if not running_linux?
         puts "Sorry. This program currently only runs in Linux."
@@ -71,10 +63,33 @@ module SimpleTestRunner
       end
     end
 
+    # Make a config file.
+    # Currently not used. I was planning on using a config file
+    # for directories to monitor and the command to run.
+    # But at the moment everything goes on the command line,
+    # and I think that's simpler.
+    # But I'm leaving the config file functions in the code,
+    # in case I decide to pick them up again.
+    def make_config_file
+      if File.exists? @options.config_file_name
+        puts "Are you sure? (y for yes)"
+        a = gets.strip.downcase
+        return unless a == "y"
+      end
+      File.open(@options.config_file_name, 'w+') do |file| 
+        file.puts @options.to_yaml
+      end
+    end
+
+    # Are we running on Linux? 
+    # returns true or false
+    # The program currently only supports Linux.
     def running_linux?
       /linux/i === RbConfig::CONFIG["host_os"]
     end
 
+    # Set up the INotify::Notifier object to watch for changes
+    # to the target dirs.
     def setup_monitor
       @notifier = INotify::Notifier.new
       @options.parsed.dirs_to_monitor.each do |dir|
@@ -82,10 +97,7 @@ module SimpleTestRunner
       end
     end
 
-    def event_of_interest?(flags)
-      flags.include?(:modify) || flags.include?(:moved_to)
-    end
-
+    # Run the command that was specified on the command line.
     def run_command
         system "#{@options.parsed.command_str}"
     end
